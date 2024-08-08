a1f233f7e0b187cd1e7a2808560a17bbaf26be10
nitatemic <alexandre@nitatemic.dev>
2024-08-08 13:39:52 +0200

    Initial Commit

---
+++ VideoSubtitleMerger/VideoSubtitleMergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoSubtitleMergerApp.swift
+//  VideoSubtitleMerger
+//
+//  Created by Alexandre de Lemeny-Makedone on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoSubtitleMergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
